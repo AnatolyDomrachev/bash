157f9fad1f3ed35d1860d28038a1539a92f38c3b
Your Name <you@example.com>
2020-09-07 12:02:42 +0300

    1

--- f.sh
+++ f.sh
@@ -3,7 +3,8 @@ func()
 	ls $1 | cat -n
 	echo Enter directory number
 	read num
-	ls $1 | cat -n | awk '{if($1==num)print $2}' num=$num
+	dir=$(ls $1 | cat -n | awk '{if($1==num)print $2}' num=$num)
+	echo $1/$dir
 }
 
 func test_dir
